2eebe50a85c7a6ad9a6b8c019ece40ad55e8747f
eykode <eykode@yandex-team.ru>
2024-12-04 21:52:52 +0700

    d4 refactoring

--- src/d4.rs
+++ src/d4.rs
@@ -4,6 +4,7 @@ const X: i8 = 1 << 0;
 const M: i8 = 1 << 1;
 const A: i8 = 1 << 2;
 const S: i8 = 1 << 3;
+const BUFFER: usize = 4;
 
 fn char_to_i8(c: char) -> i8 {
     match c {
@@ -15,102 +16,52 @@ fn char_to_i8(c: char) -> i8 {
     }
 }
 
-fn first(data: &str) -> anyhow::Result<i64> {
-    let mut matrix = Vec::<Vec<i8>>::new();
+fn make_matrix(data: &str) -> Vec<Vec<i8>> {
+    let lines = data.split("\n").collect::<Vec<_>>();
+    let m = lines.len() + BUFFER * 2;
+
+    let mut matrix = vec![vec![0i8; m]; m];
+
     for (row, line) in data.split("\n").enumerate() {
-        matrix.push(Vec::<i8>::new());
-        for char in line.chars() {
-            matrix[row].push(char_to_i8(char));
+        for (col, char) in line.chars().enumerate() {
+            matrix[row+BUFFER][col+BUFFER] = char_to_i8(char);
         }
     }
-    let n = matrix.len();
+    matrix
+}
+
+fn first(data: &str) -> anyhow::Result<i64> {
+    let matrix = make_matrix(data);
+    let m= BUFFER..matrix.len()-BUFFER;
     let xmasb: i32 = unsafe { mem::transmute([X, M, A, S]) };
     let samxb: i32 = unsafe { mem::transmute([S, A, M, X]) };
     let mut res = 0;
-    let xmas_len = 3;
-    for i in 0..n {
-        for j in 0..n {
-            if j + xmas_len < n {
-                let b: i32 = unsafe {
-                    unsafe {
-                        mem::transmute([
-                            matrix[i][j],
-                            matrix[i][j + 1],
-                            matrix[i][j + 2],
-                            matrix[i][j + 3],
-                        ])
-                    }
-                };
-                if xmasb ^ b == 0 || samxb ^ b == 0 {
-                    res += 1;
-                }
-            }
-            if i + xmas_len < n {
-                let b: i32 = unsafe {
-                    unsafe {
-                        mem::transmute([
-                            matrix[i][j],
-                            matrix[i + 1][j],
-                            matrix[i + 2][j],
-                            matrix[i + 3][j],
-                        ])
-                    }
-                };
-                if xmasb ^ b == 0 || samxb ^ b == 0 {
-                    res += 1;
-                }
-            }
+    for i in m.clone() {
+        for j in m.clone() {
+            let b: i32 = unsafe { mem::transmute([matrix[i][j], matrix[i][j+1], matrix[i][j+2], matrix[i][j+3]])};
+            if xmasb == b || samxb == b { res += 1; }
+            
+            let b: i32 = unsafe { mem::transmute([matrix[i][j], matrix[i+1][j], matrix[i+2][j], matrix[i+3][j]])};
+            if xmasb == b || samxb == b { res += 1; }
 
-            if i + xmas_len < n && j + xmas_len < n {
-                let b: i32 = unsafe {
-                    unsafe {
-                        mem::transmute([
-                            matrix[i][j],
-                            matrix[i + 1][j + 1],
-                            matrix[i + 2][j + 2],
-                            matrix[i + 3][j + 3],
-                        ])
-                    }
-                };
-                if xmasb ^ b == 0 || samxb ^ b == 0 {
-                    res += 1;
-                }
-            }
+            let b: i32 = unsafe { mem::transmute([matrix[i][j], matrix[i+1][j+1], matrix[i+2][j+2], matrix[i+3][j+3]]) };
+            if xmasb == b || samxb == b { res += 1; }
 
-            if i as i64 - xmas_len as i64 >= 0 && j + xmas_len < n {
-                let b: i32 = unsafe {
-                    unsafe {
-                        mem::transmute([
-                            matrix[i][j],
-                            matrix[i - 1][j + 1],
-                            matrix[i - 2][j + 2],
-                            matrix[i - 3][j + 3],
-                        ])
-                    }
-                };
-                if xmasb ^ b == 0 || samxb ^ b == 0 {
-                    res += 1;
-                }
-            }
+            let b: i32 = unsafe { mem::transmute([matrix[i][j], matrix[i-1][j+1], matrix[i-2][j+2], matrix[i-3][j+3]]) };
+            if xmasb == b || samxb == b { res += 1; }
         }
     }
     Ok(res)
 }
 
 fn second(data: &str) -> anyhow::Result<i64> {
-    let mut matrix = Vec::<Vec<i8>>::new();
-    for (row, line) in data.split("\n").enumerate() {
-        matrix.push(Vec::<i8>::new());
-        for char in line.chars() {
-            matrix[row].push(char_to_i8(char));
-        }
-    }
-    let n = matrix.len();
+    let matrix = make_matrix(data);
+    let m= BUFFER..matrix.len()-BUFFER;
     let mut res = 0;
-    for i in 1..n-1 {
-        for j in 1..n-1 {
-            if (matrix[i][j] == A) && matrix[i-1][j-1]|matrix[i+1][j+1] == S|M && matrix[i-1][j+1]|matrix[i+1][j-1] == S|M {
-                res += 1; 
+    for i in m.clone() {
+        for j in m.clone() {
+            if matrix[i][j] == A && matrix[i-1][j-1]|matrix[i+1][j+1] == S|M && matrix[i-1][j+1]|matrix[i+1][j-1] == S|M {
+                res += 1;
             }
         }
     }
@@ -152,5 +103,4 @@ mod tests {
         let result = second(&data).unwrap();
         assert_eq!(result, 1877);
     }
-
 }
